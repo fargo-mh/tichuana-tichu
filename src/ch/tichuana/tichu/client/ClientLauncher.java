549282ad27fc9e7dfe7f6af40a3ccc53e86301a1
fargo-mh <mw-hallman@sbcglobal.net>
2020-12-08 22:18:06 -0600

    build changes + client launcher

---
+++ src/ch/tichuana/tichu/client/ClientLauncher.java
@@ -0,0 +1,11 @@
+package ch.tichuana.tichu.client;
+
+import ch.tichuana.tichu.client.Client;
+
+public class ClientLauncher {
+    public static void main(String[] args) {
+        Client gameClient = new Client();
+        gameClient.main(args);
+    }
+
+}
